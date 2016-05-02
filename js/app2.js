5d06d308ae14a0fdd2aa9f0c7cf9799e35d249a4
Christophe Melot <christophemelot@yahoo.fr>
2016-05-02 14:14:36 +0200

    add points and position event on the canvas element

--- js/app2.js
+++ js/app2.js
@@ -13,10 +13,33 @@ window.onload = function(){
 
  drawimage(ctx,"navire.jpg",0,0,738,498);
 
+	var pieces = [],
+			offset = 0;
+	
+function writeMessage(canvas, message) {
+  ctx.clearRect(490, 10, 250, 20);
+  ctx.fillStyle="rgba(251,244,230,1)";
+  ctx.font = '12pt Calibri';
+  ctx.fillStyle = 'black';
+  ctx.fillText(message, 620, 25);
+}
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
 
-	var offset = 0;
+canvas.addEventListener('mousemove', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  var message = 'x : ' + mousePos.x + ', y :' + mousePos.y;
+  writeMessage(c, message);
+}, false);
 
-	var draw = function(ctx) {
+
+
+	var gouvernail = function(ctx) {
 		ctx.save();
 		ctx.fillStyle = "rgba(98, 134, 136, 0.6)";
 		ctx.strokeStyle = "rgba(255, 255, 255,1)";
@@ -43,51 +66,105 @@ window.onload = function(){
 		ctx.fill();
 		ctx.stroke();
 		ctx.restore();
+		console.log(ctx.isPointInPath(325, 60));
+		c.onmousemove = function (e){
+			var clickedX = e.pageX - this.offsetLeft;
+	    var clickedY = e.pageY - this.offsetTop;
+	    //console.log(clickedY);
+	    //console.log(clickedX);
+			if (ctx.isPointInPath(clickedX, clickedY)) {
+		      canvas.style.cursor = 'pointer';
+		      return;
+		  }
+		}
 	};
-function drw() {
-  offset++;
-  if(offset > 1600){
-    offset = 0
-  }
-  draw(ctx);
-  setTimeout(drw,20);
-};
-drw();
+
+
+	var courbedetambot = function(ctx) {
+		ctx.save();
+		ctx.fillStyle = "rgba(98, 134, 136, 0.6)";
+		ctx.strokeStyle = "rgba(255, 255, 255,1)";
+		ctx.miterLimit = 10;
+		ctx.beginPath();
+		ctx.moveTo(68,295);
+		ctx.lineTo(72.5,295);
+		ctx.bezierCurveTo(72.5,325.5,81.25,335.25,120.5,337.5);
+		ctx.lineTo(120.5,340.5);
+		ctx.lineTo(72.5,337.5);
+		ctx.lineTo(68,295);
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+		ctx.restore();
+	};
+	pieces.push(gouvernail);
+	pieces.push(courbedetambot);
+	console.log(pieces);
+
+gouvernail(ctx);
+courbedetambot(ctx);
+$('#canvas').on('click',function (e) {
+    var clickedX = e.pageX - this.offsetLeft;
+    var clickedY = e.pageY - this.offsetTop;
+    console.log(clickedY);
+    console.log(clickedX);
+    for (var i = 0; i < pieces.length; i++) {
+        if (clickedX < pieces[i].right && clickedX > pieces[i].left && clickedY > pieces[i].top && clickedY < pieces[i].bottom) {
+            alert ('clicked number ' + (i + 1));
+        }
+    }
+
+});
+
+
+// var offset = 0;
+// function drw() {
+//   offset++;
+//   if(offset > 1600){
+//     offset = 0
+//   }
+//   gouvernail(ctx);
+//   setTimeout(drw,20);
+// };
+// drw();
 // draw(ctx);
 
-// var circles = [];
 
-// var draw = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext) {
-//     context.beginPath();
-//     context.arc(x, y, radius, 0, 2 * Math.PI, false);
-//     context.fillStyle = fillcolor;
-//     context.fill();
-//     context.lineWidth = linewidth;
-//     context.strokeStyle = strokestyle;
-//     context.stroke();
+var circles = [];
+
+var draw = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = fillcolor;
+    context.fill();
+    context.lineWidth = linewidth;
+    context.strokeStyle = strokestyle;
+    context.stroke();
     
-//     context.fillStyle = fontcolor;
-//     context.textAlign = textalign;
-//     context.font = fonttype;
+    context.fillStyle = fontcolor;
+    context.textAlign = textalign;
+    context.font = fonttype;
     
-//     context.fillText(filltext, x, y);    
-// };
+    context.fillText(filltext, x, y);    
+};
 
-// var Circle = function(x, y, radius) {
-//     this.left = x - radius;
-//     this.top = y - radius;
-//     this.right = x + radius;
-//     this.bottom = y + radius;
-// };
+var Circle = function(x, y, radius) {
+    this.left = x - radius;
+    this.top = y - radius;
+    this.right = x + radius;
+    this.bottom = y + radius;
+};
 
-// var drawCircle = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext, circles) {
-//     draw(context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext);
-//     var circle = new Circle(x, y, radius);
-//     circles.push(circle);  
-// };
+var drawCircle = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext, circles) {
+    draw(context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext);
+    var circle = new Circle(x, y, radius);
+    circles.push(circle);  
+};
 
-// drawCircle(ctx, 300, 100, "green", 30, 5, "#003300", "white", "center", "bold 20px Arial", "1", circles);
-// drawCircle(ctx, 600, 100, "blue", 30, 5, "#003300", "white", "center", "bold 20px Arial", "2", circles);
+drawCircle(ctx, 300, 100, "green", 30, 5, "#003300", "white", "center", "bold 20px Arial", "1", circles);
+drawCircle(ctx, 600, 100, "blue", 30, 5, "#003300", "white", "center", "bold 20px Arial", "2", circles);
+
+console.log(circles);
 
 // $('#canvas').on('click',function (e) {
 //     var clickedX = e.pageX - this.offsetLeft;
