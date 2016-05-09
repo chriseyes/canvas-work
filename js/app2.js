4660e78819b31b8233966a53c6fdd26528d5027f
Christophe Melot <christophemelot@yahoo.fr>
2016-05-09 16:00:04 +0200

    mouseover on each elements

--- js/app2.js
+++ js/app2.js
@@ -2,25 +2,27 @@
 
 window.onload = function(){
 
-	var c=document.getElementById("canvas");
-  var ctx=c.getContext("2d");
+	var c = document.getElementById("canvas"),
+			ctx = c.getContext("2d"),
+  		pixelRatio = window.devicePixelRatio,
+  		mouseX = 999,
+  		mouseY = 999;
 
-if (window.devicePixelRatio > 1) {
-    var canvasWidth = c.width;
-    var canvasHeight = c.height;
 
-    c.width = canvasWidth * window.devicePixelRatio;
-    c.height = canvasHeight * window.devicePixelRatio;
+console.log("Pixel ratio : "+window.devicePixelRatio);
+// for the retina screen
+if (pixelRatio > 1) {
+    var canvasWidth = c.width,
+    		canvasHeight = c.height;
+    		
+    c.width = canvasWidth * pixelRatio;
+    c.height = canvasHeight * pixelRatio;
     c.style.width = canvasWidth;
     c.style.height = canvasHeight;
+    ctx.scale(pixelRatio, pixelRatio);
+};
 
-    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-    
-}
-
-
-
-
+//function for drawing an image
  var drawimage = function(context,src,x,y,width,height){
  		var image = new Image();
  		image.src = src;
@@ -32,27 +34,63 @@ if (window.devicePixelRatio > 1) {
 	var pieces = [],
 			offset = 0;
 	
-function writeMessage(canvas, message) {
-  ctx.clearRect(490, 10, 250, 20);
+// function for write a text
+function writeMessage(canvas, message, x, y) {
+  ctx.clearRect(490, 5, 250, 20);
   ctx.fillStyle="rgba(251,244,230,1)";
   ctx.font = '12pt Calibri';
   ctx.fillStyle = 'black';
-  ctx.fillText(message, 620, 25);
-}
+  ctx.fillText(message, x, y);
+};
+
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
     y: evt.clientY - rect.top
   };
-}
+};
 
 canvas.addEventListener('mousemove', function(evt) {
   var mousePos = getMousePos(canvas, evt);
-  var message = 'x : ' + mousePos.x + ', y :' + mousePos.y;
-  writeMessage(c, message);
+  var message = 'x : ' + mousePos.x + ', y :' + mousePos.y.toFixed(0);
+  writeMessage(c, message, 635, 20);
 }, false);
 
+// var eventSurvol = function(ctx){
+
+// 	c.addEventListener('mousemove',function(e){
+// 		var posX = e.pageX - this.offsetLeft;
+//     var posY = e.pageY - this.offsetTop;
+
+// 	});
+// };
+
+
+function findOffset(obj) {
+    var curX = 0, 
+        curY = 0;
+    console.log(curX);
+    if (obj.offsetParent) {
+        do {
+            curX += obj.offsetLeft;
+            curY += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    return {x:curX,y:curY};
+    }
+}
+
+function updateCanvas(e){
+    var pos = findOffset(canvas);
+    console.log(pos);
+    mouseX = e.pageX - pos.x;
+    mouseY = e.pageY - pos.y;
+    //console.log(mouseX);
+    // ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  	courbedetambot(ctx);
+		gouvernail(ctx);
+}
 
 
 	var gouvernail = function(ctx) {
@@ -82,19 +120,16 @@ canvas.addEventListener('mousemove', function(evt) {
 		ctx.fill();
 		ctx.stroke();
 		ctx.restore();
-		console.log(ctx.isPointInPath(325, 60));
-		c.onmousemove = function (e){
-			var clickedX = e.pageX - this.offsetLeft;
-	    var clickedY = e.pageY - this.offsetTop;
-	    //console.log(clickedY);
-	    //console.log(clickedX);
-			if (ctx.isPointInPath(clickedX, clickedY)) {
-		      canvas.style.cursor = 'pointer';
-		      return;
-		  }
-		}
+		if (ctx.isPointInPath(mouseX*pixelRatio, mouseY*pixelRatio)) {
+	    canvas.style.cursor = 'pointer';
+	    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+	    ctx.fill();
+	    console.log("dedans 1");
+	  }else{
+	  	canvas.style.cursor = 'default';
+	  };
+		
 	};
-
 
 	var courbedetambot = function(ctx) {
 		ctx.save();
@@ -112,26 +147,21 @@ canvas.addEventListener('mousemove', function(evt) {
 		ctx.fill();
 		ctx.stroke();
 		ctx.restore();
+		if (ctx.isPointInPath(mouseX*pixelRatio, mouseY*pixelRatio)) {
+	    canvas.style.cursor = 'pointer';
+	    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+	    ctx.fill();
+	    console.log("dedans 2");
+	  }else{
+	  	canvas.style.cursor = 'default';
+	  	ctx.fillStyle = "rgba(98, 134, 136, 1)";
+	  };
 	};
-	pieces.push(gouvernail);
-	pieces.push(courbedetambot);
-	console.log(pieces);
 
-gouvernail(ctx);
-courbedetambot(ctx);
-$('#canvas').on('click',function (e) {
-    var clickedX = e.pageX - this.offsetLeft;
-    var clickedY = e.pageY - this.offsetTop;
-    console.log(clickedY);
-    console.log(clickedX);
-    for (var i = 0; i < pieces.length; i++) {
-        if (clickedX < pieces[i].right && clickedX > pieces[i].left && clickedY > pieces[i].top && clickedY < pieces[i].bottom) {
-            alert ('clicked number ' + (i + 1));
-        }
-    }
-
-});
-
+	courbedetambot(ctx);
+	gouvernail(ctx);
+	// eventSurvol(ctx);
+canvas.addEventListener('mousemove',updateCanvas,false);
 
 // var offset = 0;
 // function drw() {
@@ -144,55 +174,5 @@ $('#canvas').on('click',function (e) {
 // };
 // drw();
 // draw(ctx);
-
-
-var circles = [];
-
-var draw = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext) {
-    context.beginPath();
-    context.arc(x, y, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = fillcolor;
-    context.fill();
-    context.lineWidth = linewidth;
-    context.strokeStyle = strokestyle;
-    context.stroke();
-    
-    context.fillStyle = fontcolor;
-    context.textAlign = textalign;
-    context.font = fonttype;
-    
-    context.fillText(filltext, x, y);    
-};
-
-var Circle = function(x, y, radius) {
-    this.left = x - radius;
-    this.top = y - radius;
-    this.right = x + radius;
-    this.bottom = y + radius;
-};
-
-var drawCircle = function (context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext, circles) {
-    draw(context, x, y, fillcolor, radius, linewidth, strokestyle, fontcolor, textalign, fonttype, filltext);
-    var circle = new Circle(x, y, radius);
-    circles.push(circle);  
-};
-
-drawCircle(ctx, 300, 100, "green", 30, 5, "#003300", "white", "center", "bold 20px Arial", "1", circles);
-drawCircle(ctx, 600, 100, "blue", 30, 5, "#003300", "white", "center", "bold 20px Arial", "2", circles);
-
-console.log(circles);
-
-// $('#canvas').on('click',function (e) {
-//     var clickedX = e.pageX - this.offsetLeft;
-//     var clickedY = e.pageY - this.offsetTop;
-    
-//     for (var i = 0; i < circles.length; i++) {
-//         if (clickedX < circles[i].right && clickedX > circles[i].left && clickedY > circles[i].top && clickedY < circles[i].bottom) {
-//             alert ('clicked number ' + (i + 1));
-//         }
-//     }
-// });
-
-
 
 };
